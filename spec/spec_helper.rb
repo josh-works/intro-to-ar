22bc78e4ca0e1eeb3a68d97d8b945d8905d4b23f
Josh Thompson <thompsonjoshd@gmail.com>
2017-03-17 11:14:39 -0600

    add first feature test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,10 @@ require File.expand_path('../../config/environment.rb', __FILE__)
 
 require 'database_cleaner'
 
+require 'capybara/dsl'
+
+Capybara.app = HorseApp
+
 DatabaseCleaner.strategy = :truncation
 
 RSpec.configure do |c|
@@ -14,4 +18,6 @@ RSpec.configure do |c|
   c.after(:each) do
     DatabaseCleaner.clean
   end
+
+  c.include Capybara::DSL
 end
